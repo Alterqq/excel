a392bf51c1ca621c3bf732f83ca2bb753e85f336
Maksat Khanmamedov <maxpower6@mail.ru>
2021-01-24 03:16:55 +0300

    add resize for rows and cols title

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,37 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTemplate} from '@/components/table/table.template';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown']
+    });
+  }
+
   static className = 'excel__table'
+
   toHTML() {
     return createTemplate()
+  }
+
+  onMousedown(event) {
+    const $resizer = $(event.target)
+    const $parent = $resizer.closest('[data-type="resizable"]')
+    const coords = $parent.getCoords()
+    document.onmousemove = e => {
+      if (event.target.dataset.resize === 'col') {
+        const delta = e.pageX - coords.right
+        $parent.$el.style.width = (delta + coords.width) + 'px'
+      } else if (event.target.dataset.resize === 'row') {
+        const delta = e.pageY - coords.bottom
+        $parent.$el.style.height = (delta + coords.height) + 'px'
+      }
+    }
+
+    document.onmouseup = () => {
+      document.onmousemove = null
+    }
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -35,6 +35,14 @@ class Dom {
   off(eventName, callback) {
     this.$el.removeEventListener(eventName, callback)
   }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
 }
 
 export function $(selector) {
